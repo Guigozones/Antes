a616113f806f1810fe77a75e0c5743c533da527d
Guigozones <guilhermeoliveira7962@gmail.com>
2025-07-15 18:58:40 -0300

    inciando desafio de construçao de um site de calculo de gorjetas

---
+++ index.js
@@ -0,0 +1,5 @@
+function update(){
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+}
